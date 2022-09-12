dae3218ffb3ce292bcd311aa1e014e05cb9c0d5a
AmaliaMorales <amy657452@gmail.com>
2022-09-12 18:13:52 -0500

    archivo php de practica 3 cargado

---
+++ practicas/P03/XHTML.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title> Titulo de la página </title>
+
+</head>
+<h2> Bienvenido sobre el sitio PHP 5 </h2>
+<h3>Practica P03 </h3>
+<body>
+<?php
+
+$a = "0";
+$b = "TRUE";
+$c = FALSE;
+$d = ($a OR $b);
+$e = ($a AND $c);
+$f = ($a XOR $b);
+
+$a = '0';
+ $e_float_de_a = floatval($a);
+ echo $e_float_de_a; 
+
+floatval()
+?>
+</body>
+</html>
