938783501f19660412de97e12d10899592087678
AmaliaMorales <amy657452@gmail.com>
2022-09-13 14:47:42 -0500

    cargando ejercicio cuatro

--- practicas/P03/XHTML.php
+++ practicas/P03/XHTML.php
@@ -11,12 +11,17 @@
 <h2><b> Ejercicio 1 </b></h2>
 <h3><ul><br>$_myvar;</br>  <br>$_7var;</br> <br>//myvar</br> <br>$myvar;</br> <br>$var7;</br> <br>$_element1;</br> <br>//$house*5</br> </ul></3>
 <h3><ul>Respuesta: No son validas porque no cumplen con la sintaxis.</ul><h3>
+
 <h2><b> Ejercicio 2 </b></h2>
 <h3><ul><br>ManejadorSQL,MySQL,ManejadorSQL</br> <br>$a = "ManejadorSQL";</br> <br>$b = "MySQL";</br> <br>$c = &$a;</br> <br>echo "$a,$b,$c";</br></ul></h3> 
 <h3><ul><br>ManejadorSQL,MySQL,ManejadorSQL PHP server,PHP server</br> <br>$a = "PHP server";</br> <br>$b = &$a;</br> <br>echo "$a,$b";</br></ul></h3>
+
 <h2><b> Ejercicio 3 </b></h2>
 <h3><ul><br>MySQL,5a version de PHP,Array,Array$a = "PHP5";</br><br>$z[] = &$a;</br><br>$b = "5a version de PHP";</br><br>//$c = $b*10;</br><br>//$a .= $b;</br><br>//$b *=$c;</br><br>$z[0] = "MySQL";</br><br>echo "$a,$b,$z,$z";</br></br></ul></h3>
+
 <h2><b> Ejercicio 4 </b></h2>
+<h3><ul><br>function test() {</br><br>$a = "PHP5 ";</br><br>$b = "5a version de PHP. ";</br><br>echo "$a en el ámbito global: " . $GLOBALS["a"] . "\n";</br><br> echo "$b en el ámbito simple: " . $b . "\n";}</br><br>$a = "Contenido de ejemplo:  ";</br><br>$b = "Contenido de ejemplo: ";</br><br>test();</br></ul></h3>
+
 <h2><b> Ejercicio 5 </b></h2>
 <h2><b> Ejercicio 6 </b></h2>
 <h2><b> Ejercicio 7 </b></h2>
@@ -24,14 +29,25 @@
 
 <body>
 <?php
-$a = "PHP5";
-$z[] = &$a;
-$b = "5a version de PHP";
+$$a = "PHP5 ";
+//$z[] = &$a;
+$b = "5a version de PHP. ";
 //$c = $b*10;
 //$a .= $b;
 //$b *= $c;
-$z[0] = "MySQL";
-echo "$a,$b,$z,$z";
+//$z[0] = "MySQL";
+echo "$a,$b";
+function test() {
+    $a = "PHP5 ";
+    $b = "5a version de PHP. ";
+
+    echo "$a en el ámbito global: " . $GLOBALS["a"] . "\n";
+    echo "$b en el ámbito simple: " . $b . "\n";
+}
+
+$a = "Contenido de ejemplo:  ";
+$b = "Contenido de ejemplo: ";
+test();
 
 
 ?>
