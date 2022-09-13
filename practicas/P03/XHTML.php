5a2f54bd74ac02a12c61ce2370f1d590963e2d65
AmaliaMorales <amy657452@gmail.com>
2022-09-13 16:04:54 -0500

    cargando ejercicio siete

--- practicas/P03/XHTML.php
+++ practicas/P03/XHTML.php
@@ -30,37 +30,26 @@
 $c = FALSE;<br>var_dump($c);<br>echo;<br></br> $d = ($a OR $b);<br>var_dump($d);<br>echo;<br></br> $e = ($a AND $c);<br>var_dump($e);<br>echo;<br></br> $f = ($a XOR $b);<br>var_dump($f);<br></br></ul></h3>
 
 <h2><b> Ejercicio 7 </b></h2>
-
-
+<h3><ul><br>Apache/2.4.52 (Win64) OpenSSL/1.1.1m PHP/8.1.1 The following may work on your server and return "Apache/x.x.xx" Apache/2.4.52 (Win64) OpenSSL/1.1.1m PHP/8.1.1 </br></ul></h3>
+<h3><ul>b)El nombre del sistema operativo (servidor)<br></br>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36</br></ul></h3>
+<h3><ul>c)El idioma del navegador (cliente).<br></br> es-419,es;q=0.9 </ul></h3>
 <body>
 <?php
- $a = "0";
- var_dump($a);
-
- echo "<br>";
- 
- $b = "TRUE";
- var_dump($b);
-
- echo "<br>";
- 
- $c = FALSE;
- var_dump($c);
-
- echo "<br>";
- 
- $d = ($a OR $b);
- var_dump($d);
-
- echo "<br>";
- 
- $e = ($a AND $c);
- var_dump($e);
-
- echo "<br>";
- 
- $f = ($a XOR $b);
- var_dump($f);
+//Version de apache y PHP
+if(function_exists("apache_get_version")){
+    $AVerpache = apache_get_version();
+  } else {
+    $AVerpache = "unknown";
+  }
+  print $AVerpache;
+  ?>
+  The following may work on your server and return "Apache/x.x.xx"
+  <?php
+  print $_SERVER["SERVER_SOFTWARE"];
+//Sistema Operativo
+  echo $_SERVER['HTTP_USER_AGENT']; 
+//Lenguaje
+  echo $_SERVER["HTTP_ACCEPT_LANGUAGE"];
 
 
 ?>
