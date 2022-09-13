38a6b5c5d34c7296e49db0cd480e45d0259906d8
AmaliaMorales <amy657452@gmail.com>
2022-09-13 12:05:55 -0500

    cargando ejercicio

--- practicas/P03/XHTML.php
+++ practicas/P03/XHTML.php
@@ -3,11 +3,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title> Titulo de la página </title>
+<title>P03</title>
 
 </head>
-<h2> Bienvenido sobre el sitio PHP 5 </h2>
-<h3>Practica P03 </h3>
+<h1><center><b> Practica 3 </b></center></h1>
+
+<h2><b> Ejercicio 1 </b></h2>
+<h3><ul><br>$_myvar;</br>  <br>$_7var;</br> <br>//myvar</br> <br>$myvar;</br> <br>$var7;</br> <br>$_element1;</br> <br>//$house*5</br> </ul></3>
+<h3><ul>Respuesta: No son validas porque no cumplen con la sintaxis.</ul><h3>
+<h2><b> Ejercicio 2 </b></h2>
+<h3><ul><br>ManejadorSQL,MySQL,ManejadorSQL</br> <br>$a = "ManejadorSQL";</br> <br>$b = "MySQL";</br> <br>$c = &$a;</br> <br>echo "$a,$b,$c";</br></ul></h3> 
+<h2><b> Ejercicio 3 </b></h2>
+<h2><b> Ejercicio 4 </b></h2>
+<h2><b> Ejercicio 5 </b></h2>
+<h2><b> Ejercicio 6 </b></h2>
+<h2><b> Ejercicio 7 </b></h2>
+
+
 <body>
 <?php
 
@@ -16,6 +28,8 @@ $b = "MySQL";
 $c = &$a;
 
 echo "$a,$b,$c";
+
+?>
 ?>
 </body>
 </html>
